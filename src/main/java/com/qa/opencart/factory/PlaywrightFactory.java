c222d5ea6314b89db78f413a56abbdcdd69ae53a
EhsanUlHakam <ehsan.hakam@medznmore.com>
2023-04-05 12:35:44 +0500

    Reformatted

--- src/main/java/com/qa/opencart/factory/PlaywrightFactory.java
+++ src/main/java/com/qa/opencart/factory/PlaywrightFactory.java
@@ -1,5 +1,8 @@
 package com.qa.opencart.factory;
 
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.BrowserType.LaunchOptions;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -7,113 +10,105 @@ import java.nio.file.Paths;
 import java.util.Base64;
 import java.util.Properties;
 
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.BrowserContext;
-import com.microsoft.playwright.BrowserType;
-import com.microsoft.playwright.BrowserType.LaunchOptions;
-import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
-
 public class PlaywrightFactory {
 
-	Playwright playwright;
-	Browser browser;
-	BrowserContext browserContext;
-	Page page;
-	Properties prop;
+    private static final ThreadLocal<Browser> tlBrowser = new ThreadLocal<>();
+    private static final ThreadLocal<BrowserContext> tlBrowserContext = new ThreadLocal<>();
+    private static final ThreadLocal<Page> tlPage = new ThreadLocal<>();
+    private static final ThreadLocal<Playwright> tlPlaywright = new ThreadLocal<>();
+    Playwright playwright;
+    Browser browser;
+    BrowserContext browserContext;
+    Page page;
+    Properties prop;
 
-	private static ThreadLocal<Browser> tlBrowser = new ThreadLocal<>();
-	private static ThreadLocal<BrowserContext> tlBrowserContext = new ThreadLocal<>();
-	private static ThreadLocal<Page> tlPage = new ThreadLocal<>();
-	private static ThreadLocal<Playwright> tlPlaywright = new ThreadLocal<>();
+    public static Playwright getPlaywright() {
+        return tlPlaywright.get();
+    }
 
-	public static Playwright getPlaywright() {
-		return tlPlaywright.get();
-	}
+    public static Browser getBrowser() {
+        return tlBrowser.get();
+    }
 
-	public static Browser getBrowser() {
-		return tlBrowser.get();
-	}
+    public static BrowserContext getBrowserContext() {
+        return tlBrowserContext.get();
+    }
 
-	public static BrowserContext getBrowserContext() {
-		return tlBrowserContext.get();
-	}
+    public static Page getPage() {
+        return tlPage.get();
+    }
 
-	public static Page getPage() {
-		return tlPage.get();
-	}
+    /**
+     * take screenshot
+     *
+     */
+    public static String takeScreenshot() {
+        String path = System.getProperty("user.dir") + "/screenshot/" + System.currentTimeMillis() + ".png";
+        // getPage().screenshot(new Page.ScreenshotOptions().setPath(Paths.get(path)).setFullPage(true));
 
-	public Page initBrowser(Properties prop) {
+        byte[] buffer = getPage()
+                .screenshot(
+                        new Page.ScreenshotOptions().setPath(Paths.get(path)).setFullPage(true));
+        String base64Path = Base64.getEncoder().encodeToString(buffer);
 
-		String browserName = prop.getProperty("browser").trim();
-		System.out.println("browser name is : " + browserName);
+        return base64Path;
+    }
 
-		// playwright = Playwright.create();
-		tlPlaywright.set(Playwright.create());
+    public Page initBrowser(Properties prop) {
 
-		switch (browserName.toLowerCase()) {
-		case "chromium":
-			tlBrowser.set(getPlaywright().chromium().launch(new BrowserType.LaunchOptions().setHeadless(false)));
-			break;
-		case "firefox":
-			tlBrowser.set(getPlaywright().firefox().launch(new BrowserType.LaunchOptions().setHeadless(false)));
-			break;
-		case "safari":
-			tlBrowser.set(getPlaywright().webkit().launch(new BrowserType.LaunchOptions().setHeadless(false)));
-			break;
-		case "chrome":
-			tlBrowser.set(
-					getPlaywright().chromium().launch(new LaunchOptions().setChannel("chrome").setHeadless(false)));
-			break;
-		case "edge":
-			tlBrowser.set(
-					getPlaywright().chromium().launch(new LaunchOptions().setChannel("msedge").setHeadless(false)));
-			break;	
+        String browserName = prop.getProperty("browser").trim();
+        System.out.println("browser name is : " + browserName);
 
-		default:
-			System.out.println("please pass the right browser name......");
-			break;
-		}
+        // playwright = Playwright.create();
+        tlPlaywright.set(Playwright.create());
 
-		tlBrowserContext.set(getBrowser().newContext());
-		tlPage.set(getBrowserContext().newPage());
-		getPage().navigate(prop.getProperty("url").trim());
-		return getPage();
+        switch (browserName.toLowerCase()) {
+            case "chromium":
+                tlBrowser.set(getPlaywright().chromium().launch(new BrowserType.LaunchOptions().setHeadless(false)));
+                break;
+            case "firefox":
+                tlBrowser.set(getPlaywright().firefox().launch(new BrowserType.LaunchOptions().setHeadless(false)));
+                break;
+            case "safari":
+                tlBrowser.set(getPlaywright().webkit().launch(new BrowserType.LaunchOptions().setHeadless(false)));
+                break;
+            case "chrome":
+                tlBrowser.set(getPlaywright()
+                        .chromium()
+                        .launch(new LaunchOptions().setChannel("chrome").setHeadless(false)));
+                break;
+            case "edge":
+                tlBrowser.set(getPlaywright()
+                        .chromium()
+                        .launch(new LaunchOptions().setChannel("msedge").setHeadless(false)));
+                break;
 
-	}
+            default:
+                System.out.println("please pass the right browser name......");
+                break;
+        }
 
-	/**
-	 * this method is used to initialize the properties from config file
-	 */
-	public Properties init_prop() {
+        tlBrowserContext.set(getBrowser().newContext());
+        tlPage.set(getBrowserContext().newPage());
+        getPage().navigate(prop.getProperty("url").trim());
+        return getPage();
+    }
 
-		try {
-			FileInputStream ip = new FileInputStream("./src/test/resources/config/config.properties");
-			prop = new Properties();
-			prop.load(ip);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+    /**
+     * this method is used to initialize the properties from config file
+     */
+    public Properties init_prop() {
 
-		return prop;
+        try {
+            FileInputStream ip = new FileInputStream("./src/test/resources/config/config.properties");
+            prop = new Properties();
+            prop.load(ip);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-	}
-
-	/**
-	 * take screenshot
-	 * 
-	 */
-
-	public static String takeScreenshot() {
-		String path = System.getProperty("user.dir") + "/screenshot/" + System.currentTimeMillis() + ".png";
-		//getPage().screenshot(new Page.ScreenshotOptions().setPath(Paths.get(path)).setFullPage(true));
-		
-		byte[] buffer = getPage().screenshot(new Page.ScreenshotOptions().setPath(Paths.get(path)).setFullPage(true));
-		String base64Path = Base64.getEncoder().encodeToString(buffer);
-		
-		return base64Path;
-	}
-
+        return prop;
+    }
 }
--- src/test/java/com/qa/opencart/base/BaseTest.java
+++ src/test/java/com/qa/opencart/base/BaseTest.java
@@ -1,43 +1,40 @@
 package com.qa.opencart.base;
 
-import java.util.Properties;
-
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Parameters;
-
 import com.microsoft.playwright.Page;
 import com.qa.opencart.factory.PlaywrightFactory;
 import com.qa.opencart.pages.HomePage;
 import com.qa.opencart.pages.LoginPage;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+
+import java.util.Properties;
 
 public class BaseTest {
 
-	PlaywrightFactory pf;
-	Page page;
-	protected Properties prop;
+    protected Properties prop;
+    protected HomePage homePage;
+    protected LoginPage loginPage;
+    PlaywrightFactory pf;
+    Page page;
 
-	protected HomePage homePage;
-	protected LoginPage loginPage;
+    @Parameters({"browser"})
+    @BeforeTest
+    public void setup(String browserName) {
+        pf = new PlaywrightFactory();
 
-	@Parameters({ "browser" })
-	@BeforeTest
-	public void setup(String browserName) {
-		pf = new PlaywrightFactory();
+        prop = pf.init_prop();
 
-		prop = pf.init_prop();
+        if (browserName != null) {
+            prop.setProperty("browser", browserName);
+        }
 
-		if (browserName != null) {
-			prop.setProperty("browser", browserName);
-		}
+        page = pf.initBrowser(prop);
+        homePage = new HomePage(page);
+    }
 
-		page = pf.initBrowser(prop);
-		homePage = new HomePage(page);
-	}
-
-	@AfterTest
-	public void tearDown() {
-		page.context().browser().close();
-	}
-
+    @AfterTest
+    public void tearDown() {
+        page.context().browser().close();
+    }
 }
